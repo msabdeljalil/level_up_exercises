bd5443c3e462f502a026fced2bcd4ce1a457f84f
msabdeljalil <msabdeljalil@gmail.com>
2015-01-13 09:41:29 -0600

    Added pry to config/app.rb

--- happenings/hannenin/config/application.rb
+++ happenings/hannenin/config/application.rb
@@ -26,5 +26,9 @@ module Hannenin
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config do 
+      require 'pry'
+      config.console = Pry
+    end
   end
 end
